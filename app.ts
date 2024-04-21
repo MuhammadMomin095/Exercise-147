2cc5291c351f69aa87da6ceb3eea041fad18bf0f
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-04-21 22:43:12 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,24 @@
+function fetchData(
+    callback: (error: Error | null, data?: string) => void,
+): void 
+
+{
+    const error = new Error('Failed to fetch data');
+    const data = "Some Data";
+
+    if (Math.random() > 0.5) {
+        callback(null, data);
+    } else {
+        callback(error);
+    }
+}
+
+fetchData((error, data) => {
+    if (error) {
+        console.error(error.message);
+    } else {
+        console.log(data);
+    }
+    });
+
+    
